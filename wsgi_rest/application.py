30a382b6ad36e10f4d4ad9818d9f768da23e9b4d
Fedor Soldatkin <fsoldatkin@yandex.ru>
2020-09-29 01:09:15 +0300

    Prettify typing annotations.

--- wsgi_rest/application.py
+++ wsgi_rest/application.py
@@ -1,11 +1,11 @@
-from typing import Callable, NoReturn
 from functools import wraps
+from typing import Callable, Tuple
 from urllib.parse import parse_qs
 
 from wsgi_rest.response import ERROR_404, HELLO_WORLD
 
 
-def get_request_dict_from_environ(environ):
+def get_request_dict_from_environ(environ: dict) -> dict:
     try:
         request_body_size = int(environ.get('CONTENT_LENGTH', 0))
     except ValueError:
@@ -15,6 +15,7 @@ def get_request_dict_from_environ(environ):
     path = environ.get('PATH_INFO', '/')
     query_string = environ.get('QUERY_STRING', '')
     query_params = parse_qs(query_string)
+
     request = {'method': method, 'path': path, 'query_params': query_params, 'body': request_body}
     return request
 
@@ -23,7 +24,7 @@ class App:
     def __init__(self):
         self.routes = [('/', 'GET', lambda req: HELLO_WORLD)]
 
-    def route(self, path: str, method: str) -> NoReturn:
+    def route(self, path: str, method: str):
         def wrapper(view: Callable):
             @wraps(view)
             def wrapped(*args, **kwargs):
@@ -35,19 +36,24 @@ class App:
             return wrapped
         return wrapper
 
-    def route_exists(self, path: str, method: str):
+    def route_exists(self, path: str, method: str) -> bool:
         existing_routes = [r[:2] for r in self.routes]
         if (path, method) in existing_routes:
             return True
         return False
 
-    def get_route(self, path: str, method: str):
+    def get_route(self, path: str, method: str) -> Tuple[str, str, Callable]:
         for route in self.routes:
             if route[0] == path and route[1] == method:
                 return route
         raise ValueError(f'Path {method, path} does not exists.')
 
-    def dispatch(self, request):
+    def dispatch(self, request: dict):
+        """Find and pass request to appropriate handler.
+
+        If there is no such handler, return 404 response.
+        """
+
         try:
             route = self.get_route(request['path'], request['method'])
         except ValueError:
--- wsgi_rest/response.py
+++ wsgi_rest/response.py
@@ -1,10 +1,10 @@
 import json
 from dataclasses import dataclass, field
-from typing import List, Tuple, Dict
+from typing import List, Tuple
 
 
-def get_default_headers():
-    headers: List[Tuple[str, str]] = [
+def get_default_headers() -> List[Tuple[str, str]]:
+    headers = [
         ('Server', 'GEOSAS'),
         ('Connection', 'keep-alive')
     ]
@@ -15,17 +15,17 @@ def get_default_headers():
 class Response:
     code: str
     body: str = ''
-    headers: List[Tuple] = field(default_factory=get_default_headers)
+    headers: List[Tuple[str, str]] = field(default_factory=get_default_headers)
 
 
 @dataclass
 class JsonResponse:
     code: str
-    json_dict: Dict = field(default_factory=lambda: {})
-    headers: List[Tuple] = field(default_factory=get_default_headers)
+    json_dict: dict = field(default_factory=lambda: {})
+    headers: List[Tuple[str, str]] = field(default_factory=get_default_headers)
 
     @property
-    def body(self):
+    def body(self) -> str:
         return json.dumps(self.json_dict)
 
 
