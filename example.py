7fcb889ca3f30ccdd240d6a4c19340e1f76802cc
Fedor Soldatkin <fsoldatkin@yandex.ru>
2020-09-29 04:24:45 +0300

    Fix errors in example.py.

--- example.py
+++ example.py
@@ -1,17 +1,18 @@
 from wsgi_rest.application import App
 from wsgi_rest.responses import Response, JsonResponse
+from wsgi_rest.http import HTTP_200_OK
 
 app = App()
 
 
 @app.route('/echo', 'GET')
 def echo_view(request):
-    return Response(code='200', body=f'{request["query_params"]}')
+    return Response(code=HTTP_200_OK, body=f'{request.query_params}')
 
 
 @app.route('/echo', 'POST')
 def echo_view(request):
-    return JsonResponse(code='200', json_data={'request_body': request["body"]})
+    return JsonResponse(code=HTTP_200_OK, json_data={'request_body': request.query_params})
 
 
 application = app.get_application()
